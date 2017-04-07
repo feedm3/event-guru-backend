398e4aefa1ebe88d42d459a9994e5ae9b2a3634c
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2017-04-07 13:08:39 +0200

    Improve email

--- src/mail/mail.js
+++ src/mail/mail.js
@@ -24,8 +24,8 @@ const formatMailSubject = (event) => {
 const formatMailHtml = (username, event) => {
     return `Hi ${username}, <br><br>` +
         'you currently used Event Guru on Amazon Alexa® and requested more information to a concert. Here it is:<br><br>' +
-        `<h4>${event.artist}</h4> <br>` +
-        `<b>${event.venue}, ${event.city}</b><br>` +
+        `<h3>${event.artist}</h3> <br>` +
+        `<b>${event.venue}, ${capitalizeEveryFirstWord(event.city)}</b><br>` +
         `${event.dateUser}<br>` +
         `<a href="${event.shortUrl}">${event.shortUrl}</a><br>` +
         `${event.poweredBy} <br><br>` +
@@ -37,10 +37,15 @@ const formatMailText = (username, event) => {
     return 'Hi ' + username + ',\r\n\r\n' +
         'you currently used Event Guru on Amazon Alexa® and requested more information to a concert. Here it is:\r\n\r\n' +
         event.artist + '\r\n' +
-        event.venue + ', ' + event.city + '\r\n' +
+        event.venue + ', ' + capitalizeEveryFirstWord(event.city) + '\r\n' +
         event.dateUser + '\r\n' +
         event.shortUrl + '\r\n' +
         event.poweredBy + '\r\n\r\n' +
         'Have a nice day,' + '\r\n' +
         'Event Guru'
 };
+
+const capitalizeEveryFirstWord = (text) => {
+    return text.replace(/(^| )(\w)/g, (x) => x.toUpperCase());
+};
+
