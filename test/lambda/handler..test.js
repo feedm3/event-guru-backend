ac3a24ab591ab2d6b71301b8afa30cfad960b7ac
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2018-05-22 09:04:15 +0200

    test: add compile tests

--- test/lambda/handler..test.js
+++
@@ -1,9 +0,0 @@
-test('events rest handler compiles', () => {
-    // just to make sure out code compiles
-    require('../../src/lambda/events-rest-handler');
-});
-
-test('events preload handler compiles', () => {
-    // just to make sure out code compiles
-    require('../../src/lambda/events-preload-handler');
-});
---
+++ test/lambda/handler.test.js
@@ -0,0 +1,19 @@
+test('events rest handler compiles', () => {
+    // just to make sure out code compiles
+    require('../../src/lambda/events-rest-handler');
+});
+
+test('events preload handler compiles', () => {
+    // just to make sure out code compiles
+    require('../../src/lambda/events-preload-handler');
+});
+
+test('events artist handler compiles', () => {
+    // just to make sure out code compiles
+    require('../../src/lambda/artist-rest-handler');
+});
+
+test('events preload handler compiles', () => {
+    // just to make sure out code compiles
+    require('../../src/lambda/url-shorter-rest-handler');
+});
